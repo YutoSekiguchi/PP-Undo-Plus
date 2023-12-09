5d89b7c27460c4caa24402d604afc161364c2178
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-09 23:20:24 +0900

    add isDisplayChangePageButton prop to PP-UndoEditor component. The reason for this patch is to hide the change page button in the editor layout.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,6 @@ import PPUndoEditor from "./components/Note/PP-UndoEditor/layout"
 
 export default function Home() {
 	return (
-		<PPUndoEditor width={'70vw'} height={'100vh'} />
+		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} />
 	)
 }

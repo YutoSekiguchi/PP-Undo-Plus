c75346b90d11029d072201a0c0914d7d2f6ebde7
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 01:20:50 +0900

    Add GoogleProvider to NextAuth providers and implement signIn callback. The reason for this patch is to enable users to sign in with their Google accounts and authenticate with NextAuth.

---
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,39 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { TLPostUserData } from "@/@types/user";
+import { signin } from "@/app/services/user";
+
+type ClientType = {
+  clientId: string;
+  clientSecret: string;
+};
+
+export const options: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    } as ClientType),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn(params) {
+      const profile = params.profile;
+      if (profile === undefined) {
+        return Promise.resolve(false);
+      }
+      const data: TLPostUserData = {
+        Name: profile.name!,
+        DisplayName: profile.name!,
+        Email: profile.email!,
+        Image: params.user.image!,
+      };
+      const userData = await signin(data);
+      userData;
+      if (userData === null || userData === undefined) {
+        return Promise.resolve(false);
+      }
+      return Promise.resolve(true);
+    },
+  },
+};

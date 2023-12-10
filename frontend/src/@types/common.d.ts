fc582c4e9ed3ee6e11d32afc946d392b8eadbeea
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 02:44:39 +0900

    Add TLUserData interface to define the structure of user data when retrieving and updating user information. The reason for this patch is to ensure that the frontend application can properly handle user data and display the correct information to the user.

---
+++ frontend/src/@types/common.d.ts
@@ -0,0 +1,5 @@
+export interface TLIDAndCreatedAtAndUpdatedAt {
+  ID: string;
+  CreatedAt: string;
+  UpdatedAt: string;
+}
--- frontend/src/@types/user.d.ts
+++ frontend/src/@types/user.d.ts
@@ -1,6 +1,11 @@
+import { TLIDAndCreatedAtAndUpdatedAt } from "./common";
+
 export interface TLPostUserData {
   Name: string;
+  Password: string;
   DisplayName: string;
   Email: string;
   Image: string;
 }
+
+export interface TLUserData extends TLPostUserData, TLIDAndCreatedAtAndUpdatedAt {}

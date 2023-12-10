efc3270b3126e154fe6ade969e849d9260f63db7
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 01:19:15 +0900

    Update frontend next.config.js to add support for different API URLs based on the environment. The reason for this patch is to ensure that the frontend application can communicate with the correct backend API depending on whether it is in a production or development environment.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  env: {
+    API_URL: process.env.MODE=="production" ? process.env.API_URL_PROD : process.env.API_URL_DEV,
+    // APP_PASS: process.env.APP_PASS,
+  },
+}
 
 module.exports = nextConfig

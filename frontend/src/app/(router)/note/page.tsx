09259cea93078c8d0756f0cfa1b5d24e15cef89c
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-10 22:21:08 +0900

    Add Note page to display the PP-UndoEditor component. The reason for this patch is to provide a page where users can interact with and edit notes using the PP-UndoEditor component.

---
+++ frontend/src/app/(router)/note/page.tsx
@@ -0,0 +1,7 @@
+import PPUndoEditor from "@/app/components/Note/PP-UndoEditor/layout"
+
+export default function Note() {
+	return (
+		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} isDemo={false} />
+	)
+}

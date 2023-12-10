353568429c6de174db0b18ff851c70b23fc63165
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 01:19:33 +0900

    Add display_name and image fields to the User model and the corresponding columns in the users table. The reason for this patch is to store additional user information such as display name and profile image.

--- backend/model/user.go
+++ backend/model/user.go
@@ -3,10 +3,12 @@ package model
 import "time"
 
 type User struct {
-	ID        int       `gorm:"primary_key;not null;autoIncrement:true"`
-	Name      string    `gorm:"not null;column:name"`
-	Email     string    `gorm:"not null;column:email"`
-	Password  string    `gorm:"not null;column:password"`
-	CreatedAt time.Time `gorm:"not null;column:created_at"`
-	UpdatedAt time.Time `gorm:"not null;column:updated_at"`
+	ID          int       `gorm:"primary_key;not null;autoIncrement:true"`
+	Name        string    `gorm:"not null;column:name"`
+	Email       string    `gorm:"not null;column:email"`
+	Password    string    `gorm:"not null;column:password"`
+	DisplayName string    `gorm:"not null;column:display_name"`
+	Image       string    `gorm:"not null;column:image"`
+	CreatedAt   time.Time `gorm:"not null;column:created_at"`
+	UpdatedAt   time.Time `gorm:"not null;column:updated_at"`
 }

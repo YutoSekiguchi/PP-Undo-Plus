f452a8b7190df9b0fb05689679fa1cda6afa2c9d
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-10 16:40:04 +0900

    Add user_id column to NoteTag model. The reason for this patch is to establish a relationship between notes tags, and users.

--- backend/model/note_tag.go
+++ backend/model/note_tag.go
@@ -5,6 +5,7 @@ import "time"
 type NoteTag struct {
 	ID        int       `gorm:"primary_key;not null;autoIncrement:true"`
 	NoteID    int       `gorm:"not null;column:note_id"`
+	UserID    int       `gorm:"not null;column:user_id"`
 	TagID     int       `gorm:"not null;column:tag_id"`
 	CreatedAt time.Time `gorm:"not null;column:created_at"`
 	UpdatedAt time.Time `gorm:"not null;column:updated_at"`

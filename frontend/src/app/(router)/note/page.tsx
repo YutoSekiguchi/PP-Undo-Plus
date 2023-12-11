8d4538b25571fe76de02480788e162c17d960f36
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 12:04:44 +0900

    Add mode parameter to Note component. This patch is necessary to allow the Note component to receive a mode parameter from the parent component, which can be used to control the behavior of the component based on the current mode.

--- frontend/src/app/(router)/note/page.tsx
+++ frontend/src/app/(router)/note/page.tsx
@@ -1,7 +1,12 @@
 import PPUndoEditor from "@/app/components/Note/PP-UndoEditor/layout"
 
-export default function Note() {
+export default function Note({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+	const mode = searchParams.mode;
 	return (
-		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} isDemo={false} />
+		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} isDemo={false} mode={mode} />
 	)
 }

2ba02ec98aad6ef1a3311b2de148069cc465dd23
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 12:05:55 +0900

    Update home page to include a welcome message and description in different languages. This patch is necessary to provide a localized experience for users based on their language preference. Additionally, add a demo button to showcase the functionality of the application.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,35 +1,65 @@
 import "./components/common/animation.css"
+import DemoButton from "./components/home/DemoButton/layout";
 import GoogleLoginButton from "./components/home/GoogleLoginButton/layout"
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const lang = searchParams.lang;
+
+  const Welcome = () => {
+    return (
+      <>
+      {
+        lang === "en"?
+        "Welcome"
+        :
+        "ようこそ"
+      }
+      </>
+    )
+  }
+
+  const Description = () => {
+    return (
+      <>
+      {
+        lang === "en"?
+        <>
+          <p>Let's use&nbsp;</p>
+          <h1 className="title">PP-Undo&nbsp;</h1>
+          <p>Plus</p>
+        </>
+        :
+        <>
+          <h1 className="title">PP-Undo&nbsp;</h1>
+          <p>Plusを使ってみましょう</p>
+        </>
+      }
+      </>
+    )
+  }
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <div className="text-xl font-bold text-center mb-2 smooth">
-          ようこそ
+          <Welcome />
         </div>
 				<img src="/logo.png" className="w-1/3 mx-auto fuwafuwa" />
 				<div className="text-center mt-4 flex justify-center items-center font-bold text-xl slow-start-smooth">
-					<h1 className="title">PP-Undo&nbsp;</h1>Plusを使ってみましょう
+          <Description />
 				</div>
-				<div className="mx-auto">
-					<GoogleLoginButton />
-				</div>
-        {/* {
-          user?
-          <>
-            <div className="text-2xl font-bold text-center">
-              {user.Name}さん
-            </div>
-            <div className="mt-4" onClick={handleStart}>
-              <SubmitButton label={"今すぐ始める"} />
-            </div>
-          </>
-          :
-          <div className="mt-4" onClick={handleLoginPage}>
-              <SubmitButton label={"ログインして始める"} />
-            </div>
-        } */}
+        <div className="flex items-center justify-center">
+          <div className="mr-4">
+            <DemoButton lang={lang} />
+          </div>
+          <div className="ml-4">
+            <GoogleLoginButton lang={lang} />
+          </div>
+        </div>
       </div>
     </main>
 	)

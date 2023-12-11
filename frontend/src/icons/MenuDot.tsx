19d5ca2be47a9edb0ee3337033044b0afa514e2a
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 20:33:08 +0900

    Refactor MenuDotIcon component to fix deprecated SVG attribute names and improve code consistency. This patch is necessary to ensure compatibility with the latest React version and maintain code quality.

--- frontend/src/icons/MenuDot.tsx
+++ frontend/src/icons/MenuDot.tsx
@@ -2,6 +2,6 @@ import { SVGProps } from "react";
 
 export function MenuDotIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.009m3.996 0h.008m3.978 0H16"></path><circle cx="12" cy="12" r="10" stroke-width="1.5"></circle></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.009m3.996 0h.008m3.978 0H16"></path><circle cx="12" cy="12" r="10" strokeWidth="1.5"></circle></g></svg>
   )
 }

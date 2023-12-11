12ee2e96be9966d0fd5d98b2acc850f1c7e0d79d
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 12:04:23 +0900

    Add RightArrow icon component. This patch is necessary to provide a custom icon for indicating the right arrow direction in the frontend application.

---
+++ frontend/src/icons/RightArrow.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function RightArrow(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M8.025 22L6.25 20.225L14.475 12L6.25 3.775L8.025 2l10 10l-10 10Z"></path></svg>
+  )
+}

518c3834ea3eacc7e02866bf2bfdf58019bf4dbc
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 20:22:04 +0900

    Add icons

---
+++ frontend/src/icons/NoteBook.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function NoteBookIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M7.5 22q-1.45 0-2.475-1.025T4 18.5v-13q0-1.45 1.025-2.475T7.5 2H18q.825 0 1.413.587T20 4v12.525q0 .2-.162.363t-.588.362q-.35.175-.55.5t-.2.75q0 .425.2.763t.55.487q.35.15.55.413t.2.562v.25q0 .425-.288.725T19 22H7.5ZM9 15q.425 0 .713-.288T10 14V5q0-.425-.288-.712T9 4q-.425 0-.712.288T8 5v9q0 .425.288.713T9 15Zm-1.5 5h9.325q-.15-.35-.237-.712T16.5 18.5q0-.4.075-.775t.25-.725H7.5q-.65 0-1.075.438T6 18.5q0 .65.425 1.075T7.5 20Z"></path></svg>
+  )
+}

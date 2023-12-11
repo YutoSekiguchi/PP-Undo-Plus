2afd7056f40dfe17c5805473611284198fab757e
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-11 12:09:55 +0900

    Add DemoButton component to the home page. This patch is necessary to provide a button for users to access the demo version of the application.

---
+++ frontend/src/app/components/home/DemoButton/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+
+interface Props {
+  lang: string | string[] | undefined;
+}
+
+export default function DemoButton(props: Props) {
+  const { lang } = props;
+  const router = useRouter();
+  const moveDemoNotePage = () => {
+    router.push(`/note?mode=demo`);
+  }
+
+  return (
+    <div className="flex justify-center items-center">
+      <div
+        className="flex justify-center items-center cursor-pointer px-4 py-5 mt-12 border rounded-lg text-gray-600 hover:bg-gray-100"
+        onClick={moveDemoNotePage}
+      >
+        {
+          lang === "en"?
+          <span className="ml-2">Try Demo</span>
+          :
+          <span className="ml-2">デモを試す</span>
+        }
+      </div>
+    </div>
+  );
+}

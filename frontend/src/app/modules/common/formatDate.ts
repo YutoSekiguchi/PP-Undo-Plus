a39f02fbde4c143d3a461fd78b01d545b286c00c
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-13 00:34:46 +0900

    Add formatDate function to format a date string into a "YYYY/MM/DD HH:MM" format. This change might be necessary to display dates in a more readable and consistent format throughout the application.

---
+++ frontend/src/app/modules/common/formatDate.ts
@@ -0,0 +1,11 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}/${month}/${day} ${hour}:${minute}`;
+}

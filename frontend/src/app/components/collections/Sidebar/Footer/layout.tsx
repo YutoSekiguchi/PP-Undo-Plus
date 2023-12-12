1d67275b60c8ddb959f061c90c7d500644242684
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-13 00:35:16 +0900

    Refactor SidebarHeader and SidebarFooter components for improved styling and functionality. This patch changes the CSS classes for the SidebarHeader and SidebarFooter components, adds cursor-pointer classes to the add and menu icons, and introduces a new dialog component for creating new collections. This change might be necessary to enhance the user experience and provide a more intuitive interface for managing collections in the sidebar.

--- frontend/src/app/components/collections/Sidebar/Footer/layout.tsx
+++ frontend/src/app/components/collections/Sidebar/Footer/layout.tsx
@@ -12,13 +12,13 @@ interface Props {
 export default function SidebarFooter(props: Props) {
   const { lang, items, selectedMenu, setSelectedMenu } = props;
   return(
-    <div className="sidebar-footer w-full fixed bottom-0 left-0 flex items-center mb-2">
+    <div className="sidebar-footer fixed bottom-0 left-0 flex items-center justify-around mb-2">
       {
         items.map((item) => {
           return (
             <div
               key={item.name}
-              className={`sidebar-footer-item ml-2 ${selectedMenu === item.name ? "sidebar-footer-item--selected" : ""}`}
+              className={`sidebar-footer-item ml-1 mr-1 ${selectedMenu === item.name ? "sidebar-footer-item--selected" : ""}`}
               onClick={() => setSelectedMenu(item.name as "notebooks" | "setting" | "help")}
             >
               <button className={`${selectedMenu === item.name ? "text-sky-500": "text-gray-400"} sidebar-footer-item--button flex items-center justify-center`}>
--- frontend/src/app/components/collections/Sidebar/Header/layout.tsx
+++ frontend/src/app/components/collections/Sidebar/Header/layout.tsx
@@ -1,17 +1,38 @@
 import { MenuDotIcon } from "@/icons/MenuDot";
 import { PlusIcon } from "@/icons/Plus";
+import { useState } from "react";
+import NewCollectionDialog from "../../NewCollectionDialog/layout";
 
-export default function SidebarHeader() {
+interface Props {
+  lang: string | string[] | undefined;
+  userID: number;
+  fetchCollectionsByUserID: () => void;
+}
+
+export default function SidebarHeader(props: Props) {
+  const { lang, userID, fetchCollectionsByUserID } = props;
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handlePlusIconClick = () => {
+    setShowDialog(true);
+  };
+
+  const handleClose = () => {
+    setShowDialog(false);
+    fetchCollectionsByUserID();
+  }
+
   return (
     <div className="sidebar-header w-full h-12">
       <div className="sidebar-header--title flex items-center justify-end mt-6 text-sky-500">
-        <div className="sidebar-header--add-icon mr-4">
+        <div className="sidebar-header--add-icon mr-4 cursor-pointer" onClick={handlePlusIconClick}>
           <PlusIcon />
         </div>
-        <div className="sidebar-header--menu-icon mr-2">
+        <div className="sidebar-header--menu-icon mr-2 cursor-pointer">
           <MenuDotIcon />
         </div>
       </div>
+      {showDialog && <NewCollectionDialog lang={lang} onClose={handleClose} userID={userID} />}
     </div>
   );
 }

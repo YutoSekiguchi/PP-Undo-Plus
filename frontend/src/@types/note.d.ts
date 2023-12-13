3bbc3f7c1ae7470f66aacc29bd1449f0398016c7
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-13 13:08:11 +0900

    Add types for TLPostNoteData and TLNoteData in note.d.ts. This change might be necessary to provide consistent and structured data types for handling note-related information in the frontend application.

--- frontend/src/@types/note.d.ts
+++ frontend/src/@types/note.d.ts
@@ -1,3 +1,5 @@
+import { TLIDAndCreatedAtAndUpdatedAt } from "./common";
+
 export interface TLStrokePressureInfoItem {
   groupID: string | number;
   avg: number;
@@ -7,3 +9,13 @@ export interface TLStrokePressureInfoItem {
 export type TLStrokePressureInfo = {
   [id: string]: TLStrokePressureInfoItem;
 }
+
+export interface TLPostNoteData {
+  NoteCollectionID: number;
+  UserID: number;
+  Title: string;
+  SvgPath: string;
+  Snapshot: string;
+}
+
+export interface TLNoteData extends TLPostNoteData, TLIDAndCreatedAtAndUpdatedAt{}

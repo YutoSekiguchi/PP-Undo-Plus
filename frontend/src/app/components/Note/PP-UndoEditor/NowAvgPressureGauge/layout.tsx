e56624eccb589d6c733e5f0c1f595ce623817c99
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-13 18:45:59 +0900

    Refactor NowAvgPressureGauge layout to adjust position based on editor camera data. This change might be necessary to ensure that the NowAvgPressureGauge is positioned correctly relative to the editor canvas when the camera is moved.

--- frontend/src/app/components/Note/PP-UndoEditor/NowAvgPressureGauge/layout.tsx
+++ frontend/src/app/components/Note/PP-UndoEditor/NowAvgPressureGauge/layout.tsx
@@ -1,16 +1,18 @@
 import { getGradientColor } from "@/app/modules/note/getGradientColor";
+import { EditorUtils } from "../util";
 
 interface Props {
   pointerPosition: { x: number, y: number };
   nowAvgPressure: number;
+  editorUtils: EditorUtils;
 }
 
 export default function NowAvgPressureGauge(props: Props) {
-  const { pointerPosition, nowAvgPressure } = props;
+  const { pointerPosition, nowAvgPressure, editorUtils } = props;
   return (
     <div className="moveNowBorderLinearProgress" style={{ 
-      left: pointerPosition.x - 60, 
-      top: pointerPosition.y - 40,
+      left: (pointerPosition.x + editorUtils.getCameraData().x)*editorUtils.getCameraData().z, 
+      top: (pointerPosition.y + editorUtils.getCameraData().y)*editorUtils.getCameraData().z - 40,
     }}>
       <div className="bar" style={{backgroundColor: getGradientColor(nowAvgPressure) + "aa", width: `${nowAvgPressure*100}px`}}></div>
     </div>

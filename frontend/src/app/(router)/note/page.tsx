157dc65f824e766a51ca451e6d1592c7d9d9a364
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-18 10:31:43 +0900

    Add `lang` and `id` parameters to the Note component in note/page.tsx. This change might be necessary to pass and access language and ID information in the Note component, allowing for more customized and targeted functionality.

--- frontend/src/app/(router)/note/page.tsx
+++ frontend/src/app/(router)/note/page.tsx
@@ -1,7 +1,13 @@
 import PPUndoEditor from "@/app/components/Note/PP-UndoEditor/layout"
 
-export default function Note() {
+export default function Note({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+	const lang = searchParams.lang;
+	const id = searchParams.id;
 	return (
-		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} isDemo={false} />
+		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} isDemo={false} lang={lang} id={id} />
 	)
 }

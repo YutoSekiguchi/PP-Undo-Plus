1c5285affcaf63cff0c8d187ffd8294104ee5159
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-19 17:13:04 +0900

    Rename `/note/demo` route to `/demo` and `/note/page` route to `/notes/[id]/page`. This change might be necessary to improve the organization and consistency of the routing paths in the application.

--- frontend/src/app/(router)/notes/[id]/page.tsx
+++ frontend/src/app/(router)/notes/[id]/page.tsx
@@ -1,4 +1,5 @@
 import PPUndoEditor from "@/app/components/Note/PP-UndoEditor/layout"
+import { useRouter } from "next/router";
 
 export default function Note({
   searchParams,
@@ -6,7 +7,9 @@ export default function Note({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
 	const lang = searchParams.lang;
-	const id = searchParams.id;
+
+	const router = useRouter();
+	const { id } = router.query;
 	return (
 		<PPUndoEditor width={'70vw'} height={'100vh'} isDisplayChangePageButton={false} isDemo={false} lang={lang} id={id} />
 	)
--- frontend/src/app/components/home/DemoButton/layout.tsx
+++ frontend/src/app/components/home/DemoButton/layout.tsx
@@ -10,7 +10,7 @@ export default function DemoButton(props: Props) {
   const { lang } = props;
   const router = useRouter();
   const moveDemoNotePage = () => {
-    router.push(`/note/demo`);
+    router.push(`/demo`);
   }
 
   return (

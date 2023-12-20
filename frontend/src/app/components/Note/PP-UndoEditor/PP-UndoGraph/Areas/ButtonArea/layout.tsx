8a2a81e00c843c3eabdbcc284d35f457ab9ee11e
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-20 21:52:43 +0900

    Refactor PP-UndoGraph ButtonArea to include LogList component. This change might be necessary to add a log history functionality to the PP-UndoGraph area, allowing users to view and track changes made to the note over time.

--- frontend/src/app/components/Note/PP-UndoEditor/PP-UndoGraph/Areas/ButtonArea/layout.tsx
+++ frontend/src/app/components/Note/PP-UndoEditor/PP-UndoGraph/Areas/ButtonArea/layout.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { EditorUtils } from "../../../util";
+import LogList from "../../LogList/layout";
 
 interface Props {
   editorUtils: EditorUtils;
   id: number;
+  width: string | number;
+  height: string | number;
+  background: string;
 }
 
 export default function ButtonArea(props: Props) {
-  const { editorUtils, id } = props;
+  const { editorUtils, id, width, height, background } = props;
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -15,15 +19,20 @@ export default function ButtonArea(props: Props) {
     setIsOpen(true);
   }
 
+  const handleClose = () => {
+    setIsOpen(false);
+  }
+
   return (
     <div className="area">
+      <LogList id={id} editorUtils={editorUtils} width={width} height={height} background={background} handleClose={handleClose} />
       <div className="title">
         <p className="text-center font-bold text-md mb-2">
           Redo
         </p>
       </div>
       <div className="button-list flex flex-col items-center">
-        <button className="history-button text-center hover:opacity-80 text-xs md:text-sm rounded-lg mt-1 mb-2 py-2 px-1 md:px-4">
+        <button className="history-button text-center hover:opacity-80 text-xs md:text-sm rounded-lg mt-1 mb-2 py-2 px-1 md:px-4" onClick={handleOpen}>
           History
         </button>
       </div>

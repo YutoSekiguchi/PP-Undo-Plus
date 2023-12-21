a29790f9dd8d8be2fbddbe9e5d3e0385e1f3070d
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-21 18:34:52 +0900

    Rename BasilCrossOutline component to CrossIcon. This change might be necessary to improve the clarity and understanding of the component`s purpose, as well as to align with naming conventions used in the application.

--- frontend/src/icons/Cross.tsx
+++ frontend/src/icons/Cross.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 
-export function BasilCrossOutline(props: SVGProps<SVGSVGElement>) {
+export function CrossIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="m8.464 15.535l7.072-7.07m-7.072 0l7.072 7.07"></path></svg>
   )

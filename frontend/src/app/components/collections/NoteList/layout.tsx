b5c0d2a6c913be9101972aa26eeae07acc5dbf0e
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-24 18:38:54 +0900

    Refactor NoteList layout to include a loading screen. This change might be necessary to improve the user experience by providing feedback and indicating that the application is currently loading.

--- frontend/src/app/components/collections/NoteList/layout.tsx
+++ frontend/src/app/components/collections/NoteList/layout.tsx
@@ -8,6 +8,8 @@ import { useRouter } from "next/navigation";
 import { TLPostNoteData } from "@/@types/note";
 import { createNote } from "@/app/lib/note";
 import { updateCollection } from "@/app/lib/collection";
+import { useEffect, useState } from "react";
+import LoadingScreen from "@/app/Loading";
 
 interface Props {
   lang: string | string[] | undefined;
@@ -18,9 +20,11 @@ export default function NoteList(props: Props) {
   const { selectedCollection } = useSelectedCollection();
   const router = useRouter();
   const { user } = useUser();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const handleAddNoteIconClick = async () => {
     if (selectedCollection === null) return;
+    setIsLoading(true);
     if (user === null) {
       alert(
         `${lang === "en" ? "Please login again" : "ログインし直してください"}`
@@ -39,6 +43,7 @@ export default function NoteList(props: Props) {
     };
     const res = await createNote(data);
     if (res === null) {
+      setIsLoading(false)
       alert(
         `${
           lang === "en" ? "Failed to create note" : "ノートの作成に失敗しました"
@@ -46,20 +51,37 @@ export default function NoteList(props: Props) {
       );
       return;
     }
-    await updateCollection(selectedCollection)
+    await updateCollection(selectedCollection);
+    setTimeout(() => {
+      setIsLoading(false);
+    }
+    , 3000);
     if (lang !== undefined) {router.push(`/notes/${res.ID}&lang=${lang}`)} else {
       router.push(`/notes/${res.ID}`);
     }
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }
+    , 2000);
+    return () => clearTimeout(timer);
+  }, [])
+
   return (
     <div className="note-list bg-gray-100">
       {
-        selectedCollection &&
+        selectedCollection&&
         <>
           <NoteListHeader selectedCollection={selectedCollection} lang={lang} handleAddNoteIconClick={handleAddNoteIconClick} />
           <NoteListMain selectedCollection={selectedCollection} lang={lang} handleAddNoteIconClick={handleAddNoteIconClick} />
         </>
+        
+      }
+      {
+        isLoading &&
+        <LoadingScreen  />
       }
     </div>
   );

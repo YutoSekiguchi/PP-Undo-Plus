b9d69c7135268e06e21418f04f37356ec5e13a1b
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-12-24 18:39:39 +0900

    Refactor LoadingScreen component to use CSS styling for the loading animation. This change might be necessary to improve the visual appearance and flexibility of the loading screen, allowing for more customization and control over the animation.

--- frontend/src/app/Loading.tsx
+++ frontend/src/app/Loading.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import styles from './LoadingScreen.module.css';
+import './LoadingScreen.css';
 
 const LoadingScreen = () => {
   return (
-    <div className={styles.loadingContainer}>
-      <div className={styles.spinner}></div>
+    <div className="loading-container">
+    <div className="loading-wrapper">
+        <div className="loading-circle circle1"></div>
+        <div className="loading-circle circle2"></div>
+        <div className="loading-circle circle3"></div>
+        <div className="loading-shadow"></div>
+        <div className="loading-shadow"></div>
+        <div className="loading-shadow"></div>
+        <span>Loading…</span>
+    </div>
     </div>
   );
 };

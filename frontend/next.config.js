2896953bbab249b435deab47470aaf28c08ecc95
YutoSekiguchi <yuutosekiguchi@gmail.com>
2024-01-22 12:47:52 +0900

    Fix NEXTAUTH_SECRET environment variable typo in frontend/next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     API_URL: process.env.MODE=="production" ? process.env.API_URL_PROD : process.env.API_URL_DEV,
     FILE_SERVER_URL: process.env.MODE=="production" ? process.env.FILE_SERVER_URL_PROD : process.env.FILE_SERVER_URL_DEV,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_NEXTAUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     VERSION: process.env.VERSION,
     // APP_PASS: process.env.APP_PASS,
   },

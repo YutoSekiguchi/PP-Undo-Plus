b272836277f23b2811cb81429a1955da524a4400
YutoSekiguchi <yuutosekiguchi@gmail.com>
2024-07-02 19:00:35 +0900

    test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -27,6 +27,12 @@ const nextConfig = withPWA({
     VERSION: process.env.VERSION,
     // APP_PASS: process.env.APP_PASS,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 });
 
 module.exports = nextConfig
